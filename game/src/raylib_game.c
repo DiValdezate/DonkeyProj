b79b58043041d2e24163297740801a0c99ea53b6
DiValdezate <147338949+DiValdezate@users.noreply.github.com>
2023-11-07 19:49:47 +0100

    first commit

---
+++ game/src/raylib_game.c
@@ -0,0 +1,106 @@
+/*******************************************************************************************
+*
+*   raylib game template
+*
+*   <Game title>
+*   <Game description>
+*
+*   This game has been created using raylib (www.raylib.com)
+*   raylib is licensed under an unmodified zlib/libpng license (View raylib.h for details)
+*
+*   Copyright (c) 2021 Ramon Santamaria (@raysan5)
+*
+********************************************************************************************/
+
+#include "raylib.h"
+#include "screens.h"    // NOTE: Declares global (extern) variables and screens functions
+
+#if defined(PLATFORM_WEB)
+    #include <emscripten/emscripten.h>
+#endif
+
+//----------------------------------------------------------------------------------
+// Shared Variables Definition (global)
+// NOTE: Those variables are shared between modules through screens.h
+//----------------------------------------------------------------------------------
+GameScreen currentScreen = LOGO;
+Font font = { 0 };
+Music music = { 0 };
+Sound fxCoin = { 0 };
+
+//----------------------------------------------------------------------------------
+// Local Variables Definition (local to this module)
+//----------------------------------------------------------------------------------
+static const int screenWidth = 800;
+static const int screenHeight = 450;
+
+// Required variables to manage screen transitions (fade-in, fade-out)
+static float transAlpha = 0.0f;
+static bool onTransition = false;
+static bool transFadeOut = false;
+static int transFromScreen = -1;
+static GameScreen transToScreen = UNKNOWN;
+
+
+
+//----------------------------------------------------------------------------------
+// Main entry point
+//----------------------------------------------------------------------------------
+int main(void)
+{
+    // Initialization
+    //---------------------------------------------------------
+    InitWindow(screenWidth, screenHeight, "raylib game template");
+
+    InitAudioDevice();      // Initialize audio device
+
+    // Load global data (assets that must be available in all screens, i.e. font)
+    font = LoadFont("resources/mecha.png");
+    music = LoadMusicStream("resources/ambient.ogg");
+    fxCoin = LoadSound("resources/coin.wav");
+
+    SetMusicVolume(music, 1.0f);
+    PlayMusicStream(music);
+
+    // Setup and init first screen
+    currentScreen = LOGO;
+    InitLogoScreen();
+
+#if defined(PLATFORM_WEB)
+    emscripten_set_main_loop(UpdateDrawFrame, 60, 1);
+#else
+    SetTargetFPS(60);       // Set our game to run at 60 frames-per-second
+    //--------------------------------------------------------------------------------------
+
+    // Main game loop
+    while (!WindowShouldClose())    // Detect window close button or ESC key
+    {
+        
+    }
+#endif
+
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    // Unload current screen data before closing
+    switch (currentScreen)
+    {
+        case LOGO: UnloadLogoScreen(); break;
+        case TITLE: UnloadTitleScreen(); break;
+        case GAMEPLAY: UnloadGameplayScreen(); break;
+        case ENDING: UnloadEndingScreen(); break;
+        default: break;
+    }
+
+    // Unload global data loaded
+    UnloadFont(font);
+    UnloadMusicStream(music);
+    UnloadSound(fxCoin);
+
+    CloseAudioDevice();     // Close audio context
+
+    CloseWindow();          // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
+
+    return 0;
+}
+
